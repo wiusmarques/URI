b624b73752deef8b31a210b31bc32583fb6f46c6
Wiusmarques Moreira dos Reis <wiusmarques.dev@outlook.com>
2018-09-09 02:59:44 -0300

    Alteração nas informações de contato

--- STRINGS/1024.cs
+++ STRINGS/1024.cs
@@ -2,7 +2,7 @@
  * URI Online Judge | 1024 https://www.urionlinejudge.com.br/judge/pt/problems/view/1024
  * Fatorial
  * Autor: Wiusmarques Moreira dos Reis
- * Contato: wiusmarques.dev@outlool.com
+ * Contato: wiusmarques.dev@outlook.com
  */
 using System.Text;
 using System;
