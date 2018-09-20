9d45085a1e242a94435dc6aaba35bc14483a5dd8
Wiusmarques Moreira dos Reis <wiusmarques.dev@outlook.com>
2018-09-19 22:32:08 -0300

    Inclusão da solução dos exercícios 1007 e 1197

---
+++ MATEMATICA/1197.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * URI Online Judge | 1197 https://www.urionlinejudge.com.br/judge/pt/problems/view/1197
+ * Volta à Faculdade de Física
+ * Autor: Wiusmarques Moreira dos Reis
+ * Contato: wiusmarques.dev@outlook.com
+ */
+
+using System;
+
+namespace URI.MATEMATICA
+{
+    class _1197
+    {
+        private string entrada;
+        private int v, t;
+
+        public _1197()
+        {
+            StartProcess();
+        }
+
+        private void StartProcess()
+        {
+            
+            while (true)
+            {
+                entrada = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(entrada))
+                {
+                    //EOF
+                    break;
+                }
+
+                v = int.Parse(entrada.Split(' ')[0]);
+                t = int.Parse(entrada.Split(' ')[1]);
+
+                Console.WriteLine(v * t * 2);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,7 +10,23 @@ namespace URI
     {
         static void Main(string[] args)
         {
-            
+            string entrada;
+            int v, t;
+            while (true)
+            {
+                entrada = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(entrada))
+                {
+                    //EOF
+                    break;
+                }
+
+                v = int.Parse(entrada.Split(' ')[0]);
+                t = int.Parse(entrada.Split(' ')[1]);
+
+                Console.WriteLine(v * t * 2);
+            }
         }
 
     }
